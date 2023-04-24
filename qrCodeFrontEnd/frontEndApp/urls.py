55e11e7e5edb31ea6a4942aeee85837463b03f22
Enkhzaya0221 <sw20d024@mandakh.edu.mn>
2023-04-24 10:36:56 +0800

    forgot page

--- qrCodeFrontEnd/frontEndApp/urls.py
+++ qrCodeFrontEnd/frontEndApp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('forgot1/', views.forgot1),
+    path('forgot2/', views.forgot2),
+    path('forgot3/', views.forgot3),
 ]
--- qrCodeFrontEnd/frontEndApp/views.py
+++ qrCodeFrontEnd/frontEndApp/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(template_name = 'index.html',request = request)
+
+def forgot1(request):
+    return render(template_name = 'forgot/forgot1.html',request = request)
+
+def forgot2(request):
+    return render(template_name = 'forgot/forgot2.html',request = request)
+
+def forgot3(request):
+    return render(template_name = 'forgot/forgot3.html',request = request)
