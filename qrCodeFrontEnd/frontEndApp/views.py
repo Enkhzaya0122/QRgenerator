921acf2e24af40ce605b7ee1f481fd5336365f05
LOkaa <altanbayaraltansuh11@gmail.com>
2023-05-01 16:26:09 +0800

    done with download function o nfrontend

--- qrCodeFrontEnd/frontEndApp/views.py
+++ qrCodeFrontEnd/frontEndApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework.decorators import api_view
 import requests
 import json
+import base64
 
 @api_view(["POST","GET"])
 def headerBase(request):
@@ -9,7 +11,7 @@ def headerBase(request):
 
 def index(request):
     context = {}
-    if request.method == 'GET': 
+    if request.method == 'GET':
         if request.GET.get('submit'):
             
             requestJSON =   {
@@ -29,7 +31,19 @@ def index(request):
             context =   {
                             'mymembers': jsonFile['data'],
                         }
-           
+    elif request.method == "POST":
+        print("Hello")
+        if request.POST.get('download'):
+            base64_string = str(request.POST.get('base64_code'))
+            print(base64_string)
+            image_bytes = base64.b64decode(base64_string)
+            filename = "image.png"
+            response = HttpResponse(content_type='image/png')
+            response['Content-Disposition'] = f'attachment; filename="{filename}"'
+            response.write(image_bytes)
+            context =   {
+                            'mymembers': response,
+                        }
     return render(template_name = 'index.html',request = request,context=context)
 
 
